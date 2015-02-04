edd6aed12669723972e8c383a6d4587c01393b3b
Kevin Lint <kevin@flowroute.com>
2015-02-03 17:26:22 -0800

    final tweaks

--- tekt/controllers/pages.py
+++ tekt/controllers/pages.py
@@ -71,6 +71,36 @@ def read_page(id):
     return render_template("pages/read.html", page=record, section='pages')
 
 
+@blueprint.route('/<int:id>/paths', methods=['GET', 'POST'])
+def path_schedule(id):
+
+    """ view and add pages """
+
+    # record = tektonik.read_path(id)['result']
+    # data = {'path_id': id, 'page_id': 0}
+    # form = forms.PathPageForm(request.form, data=data)
+
+    # if request.method == 'POST':
+    #     new_record = tektonik.create_path_page(request.form)
+    #     is_valid = forms.is_valid(
+    #         form,
+    #         new_record,
+    #         {'page_id': 'page_selector'}
+    #     )
+    #     if is_valid:
+    #         messaging.flash(messaging.page_schedule_success)
+    #         return redirect(url_for('.page_schedule', id=id))
+    #     else:
+    #         messaging.flash(messaging.page_schedule_error)
+
+    # return render_template(
+    #     'paths/page_schedule.html',
+    #     path=record,
+    #     form=form,
+    #     section='paths'
+    # )
+
+
 @blueprint.route('/<int:id>/update', methods=['GET', 'POST'])
 def update_page(id):
 
